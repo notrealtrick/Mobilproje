27d7a7c7bc0c0af176f133e1dd58e20b205cb407
enesagu <97469455+enesagu@users.noreply.github.com>
2023-06-06 20:20:17 +0300

    Test veri tabanı işlemleri

--- app/src/main/java/com/example/mobilproje/InfoActivity.java
+++ app/src/main/java/com/example/mobilproje/InfoActivity.java
@@ -45,15 +45,7 @@ public class InfoActivity extends AppCompatActivity {
         catch (Exception e){
             e.printStackTrace();
         }
-        try {
-            database = this.openOrCreateDatabase("AfetDB", MODE_PRIVATE,null);
 
-            database.execSQL("CREATE TABLE IF NOT EXISTS testler (id INT, afetAd VARCHAR, Soru VARCHAR, cevapA VARCHAR, cevapB VARCHAR, cevapC VARCHAR, cevapD VARCHAR, cevap VARCHAR)");
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
         ekleButon = findViewById(R.id.ekleButon);
         ekleButon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,9 +57,6 @@ public class InfoActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Kayıt Başarıyla Eklendi", Toast.LENGTH_LONG).show();
                     veriAlma();
 
-                    database.execSQL("INSERT INTO testler (id,afetAd, Soru,cevapA,cevapB,cevapC,cevapD,cevap)  VALUES(0,'Deprem','Aşağıdakilerden hangisi bir deprem şiddeti olamaz? ','20.5','6.5','4.5','3.6','8.1')");
-                    Toast.makeText(getApplicationContext(), "Soru eklendi", Toast.LENGTH_LONG).show();
-                    veriAlma2();
 
 
                 }
@@ -75,15 +64,7 @@ public class InfoActivity extends AppCompatActivity {
                     e.printStackTrace();
 
                 }
-                try {
 
-
-
-                }
-                catch (Exception e){
-                    e.printStackTrace();
-
-                }
             }
         });
 
@@ -119,30 +100,6 @@ public class InfoActivity extends AppCompatActivity {
 
 
         cursor.close();
-
-
-
-    }
-    public void veriAlma2(){
-        Cursor cursor1 = database.rawQuery("SELECT * FROM testler",null);
-
-        int idIndex = cursor1.getColumnIndex("id");
-        int AFETadIndex = cursor1.getColumnIndex("afetAd");
-        int soruIndex = cursor1.getColumnIndex("Soru");
-
-        int cevapAIndex = cursor1.getColumnIndex("cevapA");
-        int cevapBIndex = cursor1.getColumnIndex("cevapB");
-        int cevapCIndex = cursor1.getColumnIndex("cevapC");
-        int cevapDIndex = cursor1.getColumnIndex("cevapD");
-        int cevapIndex = cursor1.getColumnIndex("cevap");
-
-
-        while (cursor1.moveToNext()) {
-            System.out.println("Cevap D: "+cursor1.getString(AFETadIndex));
-        }
-
-
-        cursor1.close();
 
 
 
